0c593270ef8bccbd5e63ec63cdc6d9bb3e3ee29a
Đào Công Kha <leeminhovn2k4@gmail.com>
2024-09-20 22:15:03 +0700

    remove navigation

--- app/src/main/java/com/example/java_story_bk/MainActivity.java
+++ app/src/main/java/com/example/java_story_bk/MainActivity.java
@@ -14,7 +14,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 import androidx.viewpager2.widget.ViewPager2;
 
-import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.example.java_story_bk.adapters.Viewpager2AdapterMain;
 import com.example.java_story_bk.services.AccountService;
 import com.example.java_story_bk.services.MainServices;
@@ -49,16 +48,11 @@ public class MainActivity extends AppCompatActivity {
         Viewpager2AdapterMain viewpager2AdapterMain = new Viewpager2AdapterMain(this);
         mViewPager2.setAdapter(viewpager2AdapterMain);
 
-        MeowBottomNavigation bottomNavigation = findViewById(R.id.meowBottomNavigation);
-        bottomNavigation.add(new MeowBottomNavigation.Model(dashboard, R.drawable.dashboard_icon_black));
-        bottomNavigation.add(new MeowBottomNavigation.Model(follow_stories_page, R.drawable.book_love_black));
-        bottomNavigation.add(new MeowBottomNavigation.Model(history_read, R.drawable.book_note_black));
-        bottomNavigation.add(new MeowBottomNavigation.Model(account, R.drawable.account));
+
         mViewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
                 super.onPageSelected(position);
-                handelChangePageController(position + 1, bottomNavigation);
             }
         });
         if (accountService.checkLoginAccount()) {
@@ -67,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onResponse(Call<Integer> call, Response<Integer> response) {
                     if (response.isSuccessful()) {
                         if (response.body() > 0) {
-                            bottomNavigation.setCount(follow_stories_page, response.body().toString());
+//                            bottomNavigation.setCount(follow_stories_page, response.body().toString());
 
                         }
                     }
@@ -82,36 +76,12 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
-        bottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
-            @Override
-            public Unit invoke(MeowBottomNavigation.Model model) {
-                handleChangeItemNavigation(model.getId() - 1, mViewPager2);
-                return null;
-            }
-        });
+
 
     }
 
 
-    private void handelChangePageController(int pageIndex, MeowBottomNavigation bottomNavigation) {
-        switch (pageIndex) {
-            case dashboard: {
-
-                break;
-            }
-            case follow_stories_page: {
-                break;
-            }
-            case history_read: {
-                break;
-            }
-            case account: {
-                break;
-            }
-        }
-        bottomNavigation.show(pageIndex, true);
-    }
-
+   
     private void handleChangeItemNavigation(int pageIndex, ViewPager2 mViewPager2) {
         mViewPager2.setCurrentItem(pageIndex);
     }
