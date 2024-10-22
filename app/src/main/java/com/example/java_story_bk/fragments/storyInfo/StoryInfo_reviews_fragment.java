5a001753d441d31abf19d9e69c8626070eeae838
Haovodoi <haovodoi2004@gmail.com>
2024-10-23 00:08:46 +0700

    Thông điệp commit của bạn

--- app/src/main/java/com/example/java_story_bk/fragments/storyInfo/StoryInfo_reviews_fragment.java
+++ app/src/main/java/com/example/java_story_bk/fragments/storyInfo/StoryInfo_reviews_fragment.java
@@ -3,10 +3,13 @@ package com.example.java_story_bk.fragments.storyInfo;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ScrollView;
 
 import com.example.java_story_bk.R;
@@ -15,6 +18,9 @@ import com.example.java_story_bk.models.StoryInfo;
 public class StoryInfo_reviews_fragment extends Fragment {
 
     private StoryInfo storyInfo;
+    Button bt;
+    EditText ed;
+    RecyclerView recy;
 
 
     public StoryInfo_reviews_fragment( StoryInfo storyInfo) {
@@ -25,6 +31,17 @@ public class StoryInfo_reviews_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_story_info_reviews_fragment, container, false);
+        View view=inflater.inflate(R.layout.fragment_story_info_reviews_fragment, container, false);
+       bt=view.findViewById(R.id.btrv);
+       ed=view.findViewById(R.id.edrv);
+       recy=view.findViewById(R.id.recyrate);
+       bt.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               String t=ed.getText().toString();
+
+           }
+       });
+        return view;
     }
 }
